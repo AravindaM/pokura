52667a71f00f7b0a99a975ea75ae7fe9289b1b0e
hpmtissera <hpmtissera@99f6c116-9f67-21b5-38fb-3375474ea4e7>
2012-07-09 10:52:25 +0000

    added ZookeeperStateManager

--- axis2/modules/clustering/src/org/apache/axis2/clustering/zookeeper/ZookeeperStateManager.java
+++ axis2/modules/clustering/src/org/apache/axis2/clustering/zookeeper/ZookeeperStateManager.java
@@ -19,6 +19,7 @@
 package org.apache.axis2.clustering.zookeeper;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -26,91 +27,111 @@ import org.apache.axiom.om.OMElement;
 import org.apache.axis2.AxisFault;
 import org.apache.axis2.clustering.ClusteringFault;
 import org.apache.axis2.clustering.state.StateClusteringCommand;
+import org.apache.axis2.clustering.state.StateClusteringCommandFactory;
 import org.apache.axis2.clustering.state.StateManager;
+import org.apache.axis2.clustering.state.commands.StateClusteringCommandCollection;
 import org.apache.axis2.context.AbstractContext;
 import org.apache.axis2.context.ConfigurationContext;
+import org.apache.axis2.context.ServiceContext;
+import org.apache.axis2.context.ServiceGroupContext;
 import org.apache.axis2.description.Parameter;
 
-public class ZookeeperStateManager implements StateManager{
+public class ZookeeperStateManager implements StateManager {
+
+	private final Map<String, Parameter> parameters = new HashMap<String, Parameter>();
+
+	private final Map<String, List> excludedReplicationPatterns = new HashMap<String, List>();
+
+	public ZookeeperStateManager() {
+	}
 
 	public void addParameter(Parameter param) throws AxisFault {
-		// TODO Auto-generated method stub
-		
+		parameters.put(param.getName(), param);
 	}
 
 	public void removeParameter(Parameter param) throws AxisFault {
-		// TODO Auto-generated method stub
-		
+		parameters.remove(param.getName());
 	}
 
 	public void deserializeParameters(OMElement parameterElement)
 			throws AxisFault {
-		// TODO Auto-generated method stub
-		
+		throw new UnsupportedOperationException();
+
 	}
 
 	public Parameter getParameter(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		return parameters.get(name);
 	}
 
 	public ArrayList<Parameter> getParameters() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Parameter> list = new ArrayList<Parameter>();
+		for (String msg : parameters.keySet()) {
+			list.add(parameters.get(msg));
+		}
+		return list;
 	}
 
 	public boolean isParameterLocked(String parameterName) {
-		// TODO Auto-generated method stub
-		return false;
+		return getParameter(parameterName).isLocked();
 	}
 
 	public void updateContext(AbstractContext context) throws ClusteringFault {
-		// TODO Auto-generated method stub
-		
+		StateClusteringCommand cmd = StateClusteringCommandFactory
+				.getUpdateCommand(context, excludedReplicationPatterns, false);
+		if (cmd != null) {
+			//sender.sendToGroup(cmd);
+		}
+
 	}
 
 	public void updateContext(AbstractContext context, String[] propertyNames)
 			throws ClusteringFault {
-		// TODO Auto-generated method stub
-		
+        StateClusteringCommand cmd =
+                StateClusteringCommandFactory.getUpdateCommand(context, propertyNames);
+        if (cmd != null) {
+            //sender.sendToGroup(cmd);
+        }
 	}
 
 	public void updateContexts(AbstractContext[] contexts)
 			throws ClusteringFault {
-		// TODO Auto-generated method stub
-		
+        StateClusteringCommandCollection cmd =
+                StateClusteringCommandFactory.getCommandCollection(contexts,
+                                                                   excludedReplicationPatterns);
+        if (!cmd.isEmpty()) {
+           //sender.sendToGroup(cmd);
+        }
+
 	}
 
 	public void replicateState(StateClusteringCommand command)
 			throws ClusteringFault {
-		// TODO Auto-generated method stub
-		
+		 //sender.sendToGroup(command);
 	}
 
 	public void removeContext(AbstractContext context) throws ClusteringFault {
-		// TODO Auto-generated method stub
-		
+        StateClusteringCommand cmd = StateClusteringCommandFactory.getRemoveCommand(context);
+        //sender.sendToGroup(cmd);
 	}
 
 	public boolean isContextClusterable(AbstractContext context) {
-		// TODO Auto-generated method stub
-		return false;
+	      return (context instanceof ConfigurationContext) ||
+	               (context instanceof ServiceContext) ||
+	               (context instanceof ServiceGroupContext);
 	}
 
 	public void setConfigurationContext(
 			ConfigurationContext configurationContext) {
-		// TODO Auto-generated method stub
-		
+		// nothing to do here
+
 	}
 
 	public void setReplicationExcludePatterns(String contextType, List patterns) {
-		// TODO Auto-generated method stub
-		
+		 excludedReplicationPatterns.put(contextType, patterns);
 	}
 
 	public Map getReplicationExcludePatterns() {
-		// TODO Auto-generated method stub
-		return null;
+		return excludedReplicationPatterns;
 	}
 
 }
