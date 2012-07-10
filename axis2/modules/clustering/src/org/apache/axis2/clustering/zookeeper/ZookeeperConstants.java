5bff0fde1c31d4ae16c097c3f271a64129677166
hpmtissera <hpmtissera@99f6c116-9f67-21b5-38fb-3375474ea4e7>
2012-07-10 12:10:54 +0000

    added test ComandTest

--- axis2/modules/clustering/src/org/apache/axis2/clustering/zookeeper/ZookeeperConstants.java
+++ axis2/modules/clustering/src/org/apache/axis2/clustering/zookeeper/ZookeeperConstants.java
@@ -22,4 +22,5 @@ package org.apache.axis2.clustering.zookeeper;
 public class ZookeeperConstants {
 
 	public static final String COMMAND_BASE_NAME = "/command/command";
+	public static final String COMMANDS_BASE_NAME = "/command";
 }
